90e71ae5f3edb91d7f54cd892ee9f1efc94cc04d
Behzad Fazelasl <44834067+behzad-fz@users.noreply.github.com>
2023-09-05 22:38:06 +0200

    Revert "separate user auth from customer auth"

--- src/main/java/com/bank/modules/customer/entity/Customer.java
+++ src/main/java/com/bank/modules/customer/entity/Customer.java
@@ -8,13 +8,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -23,7 +20,7 @@ import java.util.List;
 @Data // adds methods like toString and...
 @NoArgsConstructor
 @AllArgsConstructor
-public class Customer implements UserDetails {
+public class Customer {
 
 
     @Id
@@ -55,12 +52,6 @@ public class Customer implements UserDetails {
     @Column(name = "phone_number", length = 20, nullable = false)
     private String phoneNumber;
 
-    @Column(name = "username", length = 50, nullable = false, unique = true)
-    private String username;
-
-    @Column(name = "password", length = 50, nullable = false)
-    private String password;
-
     @PrePersist
     private void generateUuid() {
         if (UUID == null) {
@@ -76,35 +67,4 @@ public class Customer implements UserDetails {
 
     @OneToMany(mappedBy = "customer")
     private List<Recipient> recipients = new ArrayList<>();
-
-    @Enumerated(EnumType.STRING)
-    private Role role;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-
-    @OneToMany(mappedBy = "customer")
-    private List<CustomerToken> tokens;
 }
--- src/main/java/com/bank/modules/customer/repository/CustomerRepository.java
+++ src/main/java/com/bank/modules/customer/repository/CustomerRepository.java
@@ -5,13 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Customer findByUUID(String uuid);
-
-    Optional<Customer> findByUsername(String username);
 
     List<Customer> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCaseOrEmailContainingIgnoreCase(
             String firstName, String lastName, String email
