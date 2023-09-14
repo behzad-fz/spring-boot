4d8513ed1b89f71f83b2d6f76b1668d34c91cdaa
Behzad Fazelasl <behzad.fazelasl@gmail.com>
2023-09-14 22:53:57 +0200

    remove duplicate code

--- src/main/java/com/bank/config/ApplicationConfig.java
+++ src/main/java/com/bank/config/ApplicationConfig.java
@@ -6,7 +6,6 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
--- src/main/java/com/bank/config/JwtAuthenticationFilter.java
+++ src/main/java/com/bank/config/JwtAuthenticationFilter.java
@@ -1,7 +1,9 @@
 package com.bank.config;
 
+import com.bank.modules.customer.repository.CustomerRepository;
 import com.bank.modules.customer.repository.CustomerTokenRepository;
 import com.bank.repository.TokenRepository;
+import com.bank.repository.UserRepository;
 import com.bank.service.TokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -12,24 +14,23 @@ import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Objects;
+import java.security.InvalidParameterException;
 
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final TokenService tokenService;
-
-    private final CustomUserDetailsService userDetailsService;
-    private final CustomCustomerDetailsService customCustomerDetailsService;
-
     private final TokenRepository tokenRepository;
     private final CustomerTokenRepository customerTokenRepository;
+    private final UserRepository userRepository;
+    private final CustomerRepository customerRepository;
 
     @Override
     protected void doFilterInternal(
@@ -53,52 +54,43 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         username = tokenService.extractUserName(jwt);
         userType = tokenService.extractUserType(jwt);
 
-        if (Objects.equals(userType, "user")) {
-            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
 
-                var isTokenValid = tokenRepository.findByToken(jwt)
-                        .map(token -> !token.isExpired() && !token.isRevoked())
-                        .orElse(false);
+            UserDetails userDetails = userDetailsService(userType).loadUserByUsername(username);
 
-                if (tokenService.isTokenValid(jwt, userDetails) && isTokenValid) {
-                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                            userDetails,
-                            null,
-                            userDetails.getAuthorities()
-                    );
+            if (tokenService.isTokenValid(jwt, userDetails) && isJwtValid(userType, jwt)) {
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
 
-                    authenticationToken.setDetails(
-                            new WebAuthenticationDetailsSource().buildDetails(request)
-                    );
+                authenticationToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
 
-                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-                }
-            }
-        } else {
-            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                UserDetails userDetails = customCustomerDetailsService.loadUserByUsername(username);
-
-                var isTokenValid = customerTokenRepository.findByToken(jwt)
-                        .map(token -> !token.isExpired() && !token.isRevoked())
-                        .orElse(false);
-
-                if (tokenService.isTokenValid(jwt, userDetails) && isTokenValid) {
-                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                            userDetails,
-                            null,
-                            userDetails.getAuthorities()
-                    );
-
-                    authenticationToken.setDetails(
-                            new WebAuthenticationDetailsSource().buildDetails(request)
-                    );
-
-                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-                }
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             }
         }
 
         filterChain.doFilter(request, response);
+    }
+
+    private UserDetailsService userDetailsService(String userType) {
+        return switch (userType) {
+            case "user" -> new CustomUserDetailsService(userRepository);
+            case "customer" -> new CustomCustomerDetailsService(customerRepository);
+            default -> throw new InvalidParameterException("User Type not supported");
+        };
+    }
+
+    private boolean isJwtValid(String userType, String jwt) {
+        return switch (userType) {
+            case "user" -> tokenRepository.findByToken(jwt).map(token -> !token.isExpired() && !token.isRevoked())
+                    .orElse(false);
+            case "customer" -> customerTokenRepository.findByToken(jwt).map(token -> !token.isExpired() && !token.isRevoked())
+                    .orElse(false);
+            default -> throw new InvalidParameterException("User Type not supported");
+        };
     }
 }
