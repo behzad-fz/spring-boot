13268f08dfe0c7f739d50be87b6b4417a44189ca
Behzad Fazelasl <behzad.fazelasl@gmail.com>
2023-09-18 23:49:20 +0200

    add api to update username and password

--- src/main/java/com/bank/modules/customer/controller/auth/CustomerAuthController.java
+++ src/main/java/com/bank/modules/customer/controller/auth/CustomerAuthController.java
@@ -2,14 +2,16 @@ package com.bank.modules.customer.controller.auth;
 
 import com.bank.controller.auth.AuthenticationRequest;
 import com.bank.controller.auth.AuthenticationResponse;
+import com.bank.modules.customer.entity.Customer;
+import com.bank.modules.customer.repository.CustomerRepository;
+import com.bank.modules.customer.request.ChangeUserNamePasswordRequest;
 import com.bank.modules.customer.service.CustomerAuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/customer-auth")
@@ -17,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerAuthController {
 
     private final CustomerAuthenticationService authenticationService;
+    private final CustomerRepository customerRepository;
+    private final PasswordEncoder encoder;
 
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(
@@ -27,5 +31,15 @@ public class CustomerAuthController {
         } catch (Exception ignored) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
+    }
+
+    @PutMapping("/update-my-credentials")
+    public ResponseEntity<Customer> changeCredentials(@RequestBody ChangeUserNamePasswordRequest request) {
+        Customer customer = (Customer) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        customer.setUsername(request.getUsername());
+        customer.setPassword(this.encoder.encode(request.getPassword()));
+        customerRepository.save(customer);
+
+        return ResponseEntity.ok(customer);
     }
 }
