c7f8907190730e8337031f34e3a0c5e983015639
Bezy Lay <bezylay@Mac.home>
2026-08-15 22:01:51 +0200

    Add transfer between own accounts (SCRUM-5)

--- src/main/java/com/bank/modules/transaction/controller/TransactionController.java
+++ src/main/java/com/bank/modules/transaction/controller/TransactionController.java
@@ -2,9 +2,11 @@ package com.bank.modules.transaction.controller;
 
 import com.bank.modules.transaction.entity.CurrencyConversionResult;
 import com.bank.modules.transaction.entity.Transaction;
+import com.bank.modules.transaction.entity.TransferResult;
 import com.bank.modules.transaction.request.CurrencyConversionRequest;
 import com.bank.modules.transaction.request.NewTransaction;
 import com.bank.modules.transaction.request.RecipientPaymentRequest;
+import com.bank.modules.transaction.request.TransferRequest;
 import com.bank.modules.transaction.service.TransactionService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -46,6 +48,16 @@ public class TransactionController {
         @PathVariable String accountUUID
     ) {
         CurrencyConversionResult result = transactionService.convertCurrency(accountUUID, request);
+
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/transfer")
+    private ResponseEntity<TransferResult> transfer(
+        @Valid @RequestBody TransferRequest request,
+        @PathVariable String accountUUID
+    ) {
+        TransferResult result = transactionService.transferBetweenAccounts(accountUUID, request);
 
         return ResponseEntity.ok(result);
     }
